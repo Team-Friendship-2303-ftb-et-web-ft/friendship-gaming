c838d6d910bd7f145834051532ccdec687d9f306
Anna Gibes <gibesav@gmail.com>
2023-05-16 13:22:45 -0500

    adds user functions

--- server/db/users.js
+++ server/db/users.js
@@ -3,13 +3,89 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-async function createUser() {}
+async function createUser({username, password}) {
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const { rows: [user] } = await client.query(`
+      INSERT INTO users(username, password)
+      VALUES ($1, $2)
+      ON CONFLICT (username) DO NOTHING
+      RETURNING id, username;
+      `, [username, hashedPassword]);
 
-async function getUser() {}
+      if (!user.length) {
+        console.log('could not find user');
+        return;
+      }
+      
+      return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-async function getUserById() {}
+async function getUser({username, password}) {
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
 
-async function getUserByUsername() {}
+    if (!passwordsMatch) {
+      console.log('passwords do not match');
+      return; 
+    } else {
+      const { rows: [user] } = await client.query(`
+      SELECT id, username FROM users
+      WHERE username = $1
+      `, [username]);
+
+      if (!user.length) {
+        console.log('could not find user');
+        return;
+      }
+
+      return user;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getUserById(userID) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT id, username FROM users
+      WHERE userID=$1
+    `, [userID]);
+
+    if (!user.length) {
+      console.log('could not find user');
+      return;
+    }
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+      SELECT * FROM users
+      WHERE username=$1
+    `, [username]);
+
+    if (!user.length) {
+      console.log('could not find user');
+      return;
+    }
+
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 module.exports = {
   createUser,

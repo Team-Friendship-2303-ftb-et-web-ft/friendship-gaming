169e55b72076b9221828eacaefd9ec9f7eb8b533
bday <brookeharris2248@gmail.com>
2023-05-16 14:48:26 -0400

    base seed info

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,54 +1,145 @@
-const client = require('./client');
+const {
+  client
+} = require('./client');
+
+
+/*******DROP TABLES ********/
 
 const dropTables = async () => {
-  try {
-    console.log('Starting to drop all tables...');
+  try{
+    console.log("Dropping tables now!")
     await client.query(`
     DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS cart;
+    DROP TABLE IF EXISTS cartItems;
+    DROP TABLE IF EXISTS tags;
+    DROP TABLE IF EXISTS userInfo;
+    DROP TABLE IF EXISTS games;
+    DROP TABLE IF EXISTS addresses;
     `);
-    console.log('Finished droppping all tables successfully!');
-  } catch (error) {
-    console.error('Error dropping tables');
+    console.log("Tables have been dropped!")
+  } catch (error){
+    console.error("Well, that didnt work...")
     throw error;
   }
-};
+}
+
+/*******CREATE TABLES ********/
 
 const createTables = async () => {
   try {
-    console.log('Starting to create all tables...');
-    await client.query(`
-    CREATE TABLE users(
-      id SERIAL PRIMARY KEY,
-      username VARCHAR(255) UNIQUE NOT NULL,
-      password VARCHAR(255) NOT NULL
-    );
-    `);
-    console.log(
-      'Finished creating all tables successfully! Now, to add some data!'
-    );
+      console.log("Found my hammer, table on the way!")
+      await client.query(`
+      CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username varchar(255) UNIQUE NOT NULL,
+          password varchar(255) NOT NULL
+        );
+        CREATE TABLE userInfo (
+          id SERIAL PRIMARY KEY,
+          "userId" INTEGER REFERENCES users(id),
+          firstName varchar(255) NOT NULL,
+          lastName varchar(255) NOT NULL,
+          dateOfBirth DATE
+          isAdmin BOOLEAN DEFAULT false,
+          "addressId" INTEGER REFERENCES addresses(id)
+        );
+        CREATE TABLE tags(
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL
+          );
+        CREATE TABLE game_tags(
+          "gameId" INTEGER REFERENCES games(id),
+          "tagId" INTEGER REFERENCES tags(id),
+          UNIQUE ("postId", "tagId")
+                  );
+        CREATE TABLE games(
+          id SERIAL PRIMARY KEY,
+          authorName varchar(255) NOT NULL,
+          genre varchar(255) NOT NULL,
+          title varchar(255) NOT NULL,
+          price DECIMAL NOT NULL,
+          description  varchar(500) NOT NULL,
+          featured BOOLEAN DEFAULT false
+        );
+        CREATE TABLE addresses(
+          id SERIAL PRIMARY KEY,
+          street_address varchar(255) NOT NULL,
+          city varchar(255) NOT NULL,
+          state varchar(255) NOT NULL,
+          country varchar(255) NOT NULL,
+          postal_code INTEGER 
+        );
+        `);
+      console.log("Tables done. Double tapped for good measure.")
   } catch (error) {
-    console.error('Error creating tables');
-    throw error;
+      console.error("Made an error making tables!")
+      throw error;
   }
-};
+}
 
-const createInitialUsers = async () => {
-  console.log('Adding initial users to "Users" table...');
-  console.log('Finished adding users!');
-};
+
+
+
+/*******CREATE USERS ********/
+
+async function createInitialUsers() {
+  try{ 
+      console.log("Creating the Users");
+      await createUser({ 
+          username: 'albert', 
+          password: 'bertie99'
+        });
+        await createUser({ 
+          username: 'sandra', 
+          password: '2sandy4me'
+        });
+        await createUser({ 
+          username: 'glamgal',
+          password: 'soglam'
+        });
+      console.log("Users are Created!")
+  
+  } catch(error) {
+      console.error("Just kidding nothing happened...");
+      throw error;
+  }
+}
 
 const rebuildDB = async () => {
   try {
-    await dropTables();
-    await createTables();
-    await createInitialUsers();
-  } catch (error) {
-    console.error('Error during rebuildDB', error);
-    throw error;
-  } finally {
-    await client.end();
-    console.log("Database has been rebuilt, and you're good to go!");
-  }
-};
+      client.connect();
 
-rebuildDB();
+      await dropTables();
+      await createTables();
+      await createInitialUsers();
+      //await createInitialGames();
+     // await createInitialTags();
+     // await createInitialAddresses();
+      
+  } catch (error) {
+     throw error;
+  }
+}
+
+/******* TESTS ********/
+
+const testDB = async () => {
+  try {
+    console.log("Testing, Testing 1,2...?")
+
+    
+    const users = await getAllUsers();
+    console.log("Result:", users);
+
+
+  } catch (error) {
+    console.error("It broke....no work...test fail");
+   throw error;
+  }
+}
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());

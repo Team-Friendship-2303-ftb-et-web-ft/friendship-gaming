378b58de718e2eb9a1829bf41a0b959687c27911
bday <brookeharris2248@gmail.com>
2023-05-19 12:00:15 -0400

    components exported

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+export { default as App} from './App';
+export { default as Admin} from './Admin';
+export { default as Cart} from './Cart';
+export { default as Error} from './Error';
+export { default as Checkout} from './Checkout';
+export { default as Games} from './Games';
+export { default as Header} from './Header';
+export { default as Home} from './Home';
+export { default as Login} from './Login';
+export { default as Profile } from "./Profile";
+export { default as Register } from "./Register";
+export { default as SearchBar } from "./SearchBar";
+export { default as SingleGame } from "./SingleGame";

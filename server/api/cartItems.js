b582746a2b15d5579fa34550de17b0f7f60c45e6
bday <brookeharris2248@gmail.com>
2023-05-18 20:57:41 -0400

    forgot to save one

---
+++ server/api/cartItems.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cartItemsRouter = express.Router();
+const { getAllCartItems } = require('../db');
+
+//get/ci
+
+//get/ci/userid
+
+//get/ci/gameid
+
+//post
+
+
+
+
+module.exports = cartItemsRouter;

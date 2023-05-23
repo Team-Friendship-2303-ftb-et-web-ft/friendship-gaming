fc16431e05eb1a93f33d6c10fb261671cbe9afec
bday <brookeharris2248@gmail.com>
2023-05-23 10:28:49 -0400

    weird errors again, pulls and pushes not working correctly

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -4,10 +4,21 @@ import {CreateGameForm} from "./index";
 import {NavLink, useNavigate} from 'react-router-dom';
 import './Admin.css'
 
-const Profile = () => {
+
+const Profile = (props) => {
+    const {isLoggedIn, currentUser} = props;
+
     return (
         <>
-        <h1>This is Profile!</h1>
+        {isLoggedIn ?
+        <h2>Welcome, {currentUser}</h2>
+        :
+        <h2>Welcome! Please login to get started!</h2>
+
+        }
+        <div className='profileBody'>
+            <h3>Order History</h3>
+        </div>
         </> 
     )  
 }
